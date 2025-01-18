48f67467650b3b3a2e0f9302b7479210dc67ecd4
Jamesllm <ronaljamesllapapascamontes@gmail.com>
2025-01-18 08:32:03 -0500

    fist commit

---
+++ index.js
@@ -0,0 +1,92 @@
+let MONEDAS = {
+  PEN: {
+    codigo_moneda: "PEN",
+    descripcion: "Nuevo Sol",
+    mn_short_dsc: "S/.",
+    singular: "sol",
+    plural: "soles",
+    centimos_singular: "céntimo",
+    centimos_plural: "céntimos",
+  },
+  USD: {
+    codigo_moneda: "USD",
+    descripcion: "US Dollar",
+    mn_short_dsc: "$",
+    singular: "dólar",
+    plural: "dólares",
+    centimos_singular: "centavo",
+    centimos_plural: "centavos",
+  },
+  EUR: {
+    codigo_moneda: "EUR",
+    descripcion: "Euro",
+    mn_short_dsc: "€",
+    singular: "euro",
+    plural: "euros",
+    centimos_singular: "céntimo",
+    centimos_plural: "céntimos",
+  },
+};
+
+let MAX_NUMERO = 999999999999;
+
+let UNIDADES =
+  ("cero",
+  "uno",
+  "dos",
+  "tres",
+  "cuatro",
+  "cinco",
+  "seis",
+  "siete",
+  "ocho",
+  "nueve");
+
+let DECENAS =
+  ("diez",
+  "once",
+  "doce",
+  "trece",
+  "catorce",
+  "quince",
+  "dieciséis",
+  "diecisiete",
+  "dieciocho",
+  "diecinueve");
+
+let DIEZ_DIEZ =
+  ("cero",
+  "diez",
+  "veinte",
+  "treinta",
+  "cuarenta",
+  "cincuenta",
+  "sesenta",
+  "setenta",
+  "ochenta",
+  "noventa");
+
+let CIENTOS =
+  ("_",
+  "ciento",
+  "doscientos",
+  "trescientos",
+  "cuatrocientos",
+  "quinientos",
+  "seiscientos",
+  "setecientos",
+  "ochocientos",
+  "novecientos");
+
+
+
+
+function numero_a_letras(number){
+    
+}
+
+const getTest = () => {
+  return console.log("test number to letters library 🚀");
+};
+
+module.exports = { getTest };
